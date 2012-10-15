bf08ab1165fea93dcafd1245bad360af0de14200
Jesse Emery <jesse@onagertech.com>
2012-10-14 19:52:01 -0700

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Djangly',
+      version='1.0',
+      description='Very simple Django app to include shareable bit.ly URLs for the URLs in your site.',
+      author='Jesse Emery',
+      author_email='jesse@jesseemery.com',
+      url='https://github.com/ejesse/djangly',
+      packages=['djangly'],
+     )
